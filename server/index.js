8c43cd5a63b2f1576e1b25060c60a01abbc50c45
Brian Kim <klrgch@Brians-MacBook-Air.local>
2017-10-04 16:00:15 -0700

    other change

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const items = require('../database-mysql');
-const tvMaze = require('../helper/tvmaze.js')
+const moviedb = require('../helper/moviedb.js')
 // var items = require('../database-mongo');
 
 const app = express();
 
-
-// UNCOMMENT FOR REACT
-app.use(express.static(__dirname + '/../react-client/dist'));
-
 app.use(bodyParser.json());
-
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/items', function (req, res) {
-	tvMaze.getInfoByTitle('Game of thrones', (data) => {
+	moviedb.getInfoByTitle('Game of thrones', (data) => {
 		console.log(JSON.parse(data))
 	})
 	
 });
+
+app.post('/submit', function (req, res) {
+	var title = req.body.topic.split(' ').join('_')
+	moviedb.getInfoByTitle(title, (data) => {
+		console.log(JSON.parse(data))
+	})
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
