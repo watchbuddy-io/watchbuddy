85f0abd43507b1ea746c64c0d2df5f4789389977
Helen Yi <helenhyi@gmail.com>
2017-10-04 15:41:00 -0700

    create schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,24 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS watchbuddy;
 
-CREATE DATABASE test;
+CREATE DATABASE watchbuddy;
 
-USE test;
+USE watchbuddy;
 
-CREATE TABLE items (
+CREATE TABLE user (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  show varchar(255),
+  start DATE DEFAULT CURDATE(),
+  monday int DEFAULT 0,
+  tuesday int DEFAULT 0,
+  wednesday int DEFAULT 0,
+  thursday int DEFAULT 0,
+  friday int DEFAULT 0,
+  saturday int DEFAULT 0,
+  sunday int DEFAULT 0,
+  days int,
+  hours int,
   PRIMARY KEY (ID)
 );
 
