47ba6ac005aa16547ac0ed2e13ca01af0f2d2787
Daniel <dktdaniel@gmail.com>
2017-10-05 12:29:06 -0700

    Rendering top 5 shows

--- react-client/src/components/ShowList.jsx
+++ react-client/src/components/ShowList.jsx
@@ -5,7 +5,15 @@ import ShowEntry from './ShowEntry.jsx';
 
 class ShowList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      shows: [{
+        firstAirDate: '2000-01-01',
+        genres: ['Daniel'],
+        image: '',
+        summary: 'Default summary'
+      }]
+    };
   }
 
   componentDidMount() {
@@ -13,66 +21,20 @@ class ShowList extends Component {
       url: '/recommend',
       method: 'GET',
       contentType: 'application/json',
-      success: data => console.log(data)
+      success: data => {
+        this.setState({shows: data});
+        console.log(data);
+      }
     });
   }
 
   render() {
     return (<Segment inverted>
    <List divided inverted verticalAlign='middle' size='medium'>
-    <List.Item>
-      <List.Content floated='right'>
-        <Button icon size='medium' inverted color='green'>
-          <Icon name='add to calendar'/>
-        </Button>
-      </List.Content>
-      <Image src='https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_UX182_CR0,0,182,268_AL_.jpg' size='small' />
-      <List.Content>
-        <ul>
-        <li>Game of Thrones</li>
-        <li>TV-MA</li>
-        <li>57min</li>
-        <li>Drama, Fantasy</li>
-        <li>TV Series (2011- )</li>
-      </ul>
-      </List.Content>
-    </List.Item>
     
-   <List.Item>
-      <List.Content floated='right'>
-        <Button icon size='medium' inverted color='green'>
-          <Icon name='add to calendar'/>
-        </Button>
-      </List.Content>
-      <Image src='https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_UX182_CR0,0,182,268_AL_.jpg' size='small' />
-      <List.Content>
-        <ul>
-        <li>Game of Thrones</li>
-        <li>TV-MA</li>
-        <li>57min</li>
-        <li>Drama, Fantasy</li>
-        <li>TV Series (2011- )</li>
-      </ul>
-      </List.Content>
-    </List.Item>
-
-    <List.Item>
-      <List.Content floated='right'>
-        <Button icon size='medium' inverted color='green'>
-          <Icon name='add to calendar'/>
-        </Button>
-      </List.Content>
-      <Image src='https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NTQ1NDg1Ml5BMl5BanBnXkFtZTgwNzY2NDA0MjI@._V1_UX182_CR0,0,182,268_AL_.jpg' size='small' />
-      <List.Content>
-        <ul>
-        <li>Game of Thrones</li>
-        <li>TV-MA</li>
-        <li>57min</li>
-        <li>Drama, Fantasy</li>
-        <li>TV Series (2011- )</li>
-      </ul>
-      </List.Content>
-    </List.Item>
+    {this.state.shows.map((show, i) => <ShowEntry show={show} key={i}/>)}
+    
+   
    
   </List>
   
