5faa999ea785ac302c9f8cc645792dde7e0fe8b7
Brian Kim <klrgch@Brians-MacBook-Air.local>
2017-10-06 10:10:27 -0700

    added search to work

--- server/index.js
+++ server/index.js
@@ -41,10 +41,33 @@ app.get('/recommend', function (req, res) {
 
 
 app.post('/search', function (req, res) {
-	console.log(req.body)
-	var title = req.body.search
-	moviedb.search(title, (data) => {
-		console.log(JSON.parse(data))
+	console.log(req.body.term)
+	var title = req.body.term
+	var genres;
+	moviedb.genre((data) => {
+		genres = JSON.parse(data).genres
+		moviedb.search(title, (data) => {
+			var searched = JSON.parse(data).results.splice(0,10);
+			var array = [];
+			searched.forEach((el) => {
+				var obj = {};
+				obj.name = el.name;
+				var arr = [];
+				el.genre_ids.forEach((int) => {
+					for (var i = 0; i < genres.length; i++) {
+						if (genres[i].id === int) {
+							arr.push(genres[i].name)
+						}
+					}
+				})
+				obj.genres = arr
+				obj.summary = el.overview;
+				obj.firstAirDate = el.first_air_date;
+				obj.image = "https://image.tmdb.org/t/p/w500/" + el.backdrop_path;
+				array.push(obj)
+			})
+			res.send(array);
+		})
 	})
 })
 
