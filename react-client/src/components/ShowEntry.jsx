a034c48773177a2a1841a053699c8968441f63d6
Daniel <dktdaniel@gmail.com>
2017-10-07 16:16:17 -0700

    Hide add show button for non logged in users

--- react-client/src/components/ShowEntry.jsx
+++ react-client/src/components/ShowEntry.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Image, List, Button, Icon, Grid } from 'semantic-ui-react';
 
-const ShowEntry = ({ show, getShow }) => (
-  <Grid.Row>
-      <Grid.Column width={3}>
-        <Container>
-        <Image src={show.image} size='big' />
-        <Button fluid icon size='big' inverted color='red' 
-        onClick={() => {
-          let showObj = {};
-          showObj.id = show.id;
-          showObj.name = show.name;
-          getShow(showObj);
-        }}>
-          Add show <Icon name='add to calendar'/>
-        </Button>
-        </Container>
-      </Grid.Column>
+class ShowEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-      <Grid.Column width={13}>
-        <ul>
-          <li><h4>{show.name}</h4></li>
-          <ul>{show.genres.map((genre, i) => <li key={i}>{genre}</li>)}</ul>
-          <li>{show.summary}</li>
-          <li>First aired: {show.firstAirDate}</li>
-        </ul>
-   </Grid.Column>
-  </Grid.Row>
-  );
+  render() {
+    return (
+      <Grid.Row>
+          <Grid.Column width={3}>
+            <Container>
+            <Image src={this.props.show.image} size='big' />
+            {this.props.loggedIn === 'true' 
+              ? <Button fluid icon size='big' inverted color='red' 
+              onClick={() => {
+                let showObj = {};
+                showObj.id = this.props.show.id;
+                showObj.name = this.props.show.name;
+                this.props.getShow(showObj);
+              }}>
+                Add show <Icon name='add to calendar'/>
+              </Button>
+              : null
+            }
+            </Container>
+          </Grid.Column>
+
+          <Grid.Column width={13}>
+            <ul>
+              <li><h4>{this.props.show.name}</h4></li>
+              <ul>{this.props.show.genres.map((genre, i) => <li key={i}>{genre}</li>)}</ul>
+              <li>{this.props.show.summary}</li>
+              <li>First aired: {this.props.show.firstAirDate}</li>
+            </ul>
+       </Grid.Column>
+      </Grid.Row>
+    )
+  }
+}
 
 export default ShowEntry;
