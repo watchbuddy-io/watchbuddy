6af958238f1a2462839603c9db7e1a84bf1a6bb4
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-10-10 19:12:49 -0700

    Implement HomeCarousel component

--- react-native-init/watchbuddy/client/src/App.js
+++ react-native-init/watchbuddy/client/src/App.js
@@ -48,7 +48,7 @@ export default class App extends Component<{}> {
 
   render() {
     return (
-        <View style={{flex: 1}}>
+        <View >
           <HomeCarousel />
         </View>
     );
--- react-native-init/watchbuddy/client/src/HomeCarousel.js
+++ react-native-init/watchbuddy/client/src/HomeCarousel.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import {
+  Dimensions,
   Platform,
   StyleSheet,
   Text,
@@ -11,67 +13,116 @@ import {
   SwipeDeck 
 } from 'react-native-elements';
 
-const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 500;
 
-// test data
 const DATA = [
-  { id: 1, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 2, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 3, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 4, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 5, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 6, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' },
-  { id: 7, text: 'Joe', age: 25, uri: 'https://ca.slack-edge.com/T6EQHDY58-U6R9RCMRB-3cc279101242-1024' }
+  {
+    id: 1,
+    genre: 'sci-fi',
+    title: 'Blade Runner 2049',
+    uri: 'https://pbs.twimg.com/media/DH9QvgAUQAAZANa.jpg:large'
+  },
+  {
+    id: 2,
+    genre: 'action',
+    title: 'Inception',
+    uri: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg'
+  },
+  {
+    id: 3,
+    genre: 'sci-fi',
+    title: 'Interstellar',
+    uri: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_UY1200_CR69,0,630,1200_AL_.jpg'
+  }
 ];
+
+const SCREEN_RATIOS = {
+  CONTAINER_HEIGHT: .75,
+  CONTAINER_WIDTH: .92,
+  IMAGE_HEIGHT: .92,
+  IMAGE_WIDTH: .915,
+  MARGIN_TOP: .05
+}
 
 export default class HomeCarousel extends Component {
   constructor() {
     super()
 
+    this.state = {
+      SCREEN_HEIGHT: Dimensions.get('window').height,
+      SCREEN_WIDTH : Dimensions.get('window').width     
+    }
+
+    this.liked = [];
+    this.disliked = [];
+    this.unwatched = [];
   }
 
   renderCard(card) {
+    let SCREEN_HEIGHT = this.state.SCREEN_HEIGHT;
+    let SCREEN_WIDTH = this.state.SCREEN_WIDTH;
+
     return (
       <Card
         key={card.id}
-        containerStyle={{borderRadius: 10, flex: 1}}
-        featuredTitle={`${card.text}, ${card.age}`}
+        containerStyle={{
+          height: SCREEN_HEIGHT * SCREEN_RATIOS.CONTAINER_HEIGHT,
+          width: SCREEN_WIDTH * SCREEN_RATIOS.CONTAINER_WIDTH,
+          marginTop: SCREEN_HEIGHT * SCREEN_RATIOS.MARGIN_TOP
+        }}
+        featuredTitle={`${card.title}`}
         featuredTitleStyle={{position: 'absolute', left: 15, bottom: 10, fontSize: 30 }}
         image={{ uri: card.uri }}
-        imageStyle={{borderRadius: 10, flex: 1}}
+        imageStyle={{
+          height: SCREEN_HEIGHT * SCREEN_RATIOS.CONTAINER_HEIGHT,
+          width: SCREEN_WIDTH * SCREEN_RATIOS.IMAGE_WIDTH
+        }}
       />
     )
   }
 
   onSwipeRight(card) {
-    console.log("Card liked: " + card.text);
+    console.log("Movie liked: " + card.title);
+    this.liked.push(card);
   }
 
   onSwipeLeft(card) {
-    console.log("Card disliked: " + card.text);
+    console.log("Movie disliked: " + card.title);
+    this.disliked.push(card);
   }
 
   renderNoMoreCards() {
+    let SCREEN_HEIGHT = this.state.SCREEN_HEIGHT;
+    let SCREEN_WIDTH = this.state.SCREEN_WIDTH;
+
     return (
       <Card
-        containerStyle={{borderRadius: 10, flex: 1}}
-        featuredTitle="No more cards"
+        containerStyle={{
+          height: SCREEN_HEIGHT * SCREEN_RATIOS.CONTAINER_HEIGHT,
+          width: SCREEN_WIDTH * SCREEN_RATIOS.CONTAINER_WIDTH,
+          marginTop: SCREEN_HEIGHT * SCREEN_RATIOS.MARGIN_TOP,
+          backgroundColor: 'aqua'
+        }}
+        featuredTitle="You're all set!"
         featuredTitleStyle={{fontSize: 25}}
-        image={{ uri: 'https://i.imgflip.com/1j2oed.jpg' }}
-        imageStyle={{borderRadius: 10, flex: 1}}
+        image={{}}
+        imageStyle={{
+          height: SCREEN_HEIGHT * SCREEN_RATIOS.CONTAINER_HEIGHT,
+          width: SCREEN_WIDTH * SCREEN_RATIOS.IMAGE_WIDTH,
+        }}
       />
     )
   }
 
   render() {
+    let SCREEN_HEIGHT = this.state.SCREEN_HEIGHT;
+
     return (
       <SwipeDeck
         data={DATA}
-        renderCard={this.renderCard}
-        renderNoMoreCards={this.renderNoMoreCards}
-        onSwipeRight={this.onSwipeRight}
-        onSwipeLeft={this.onSwipeLeft}
+        renderCard={this.renderCard.bind(this)}
+        renderNoMoreCards={this.renderNoMoreCards.bind(this)}
+        onSwipeRight={this.onSwipeRight.bind(this)}
+        onSwipeLeft={this.onSwipeLeft.bind(this)}
       />
     )
   }
