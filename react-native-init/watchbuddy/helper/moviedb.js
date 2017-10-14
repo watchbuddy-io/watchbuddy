2803426cb42f5c34637799640f7d37810611766b
Seva Mouler <vpmouler@gmail.com>
2017-10-13 20:11:10 -0700

    cleaned moviedb.js

--- react-native-init/watchbuddy/helper/moviedb.js
+++ react-native-init/watchbuddy/helper/moviedb.js
@@ -32,17 +32,14 @@ let getPopularShows = (callback) => {
 
 
 let discoverMoviesByGenre = (genres, callback) => {
-	console.log('OBJECT IMPORT', genreNames)
 	let uriString = '';
 	genres.forEach(genre => {
-		console.log('GENRE LOOP',genre);
-		console.log('GENRE NAMES OBJECT', genreNames[genre])
 		uriString += genreNames[genre] + ','; 
 	})
 
 	uriString = strictUriEncode(uriString.slice(0, uriString.length - 1))
 
-	console.log(uriString)
+	console.log('Inside movidedb.js uriString: ', uriString);
 
 	let options = {
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${config.TOKEN}&&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${uriString}`,
